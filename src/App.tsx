367df42bd5aab555ebfce746ee4e43149fe5563d
Eduardo Hidalgo <rami.edu.hi@outlook.com>
2025-10-02 21:53:41 -0500

    Update builder.yml to use dynamic SURGE_DOMAIN for deployment and modify App component header to reflect deployment method.

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Deploy with surge</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
